20ea3a1cd2689dd52cf987714f5e99f7c7197c5e
Azwan Triyadi <80760932+azwan20@users.noreply.github.com>
2023-11-11 19:17:27 +0800

    Add files via upload

---
+++ src/pages/landingPage/header.js
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import { useState } from "react";
+
+export default function Header() {
+    const [menuVisible, setMenuVisible] = useState(false); // State untuk mengontrol tampilan menu
+
+    const toggleMenu = () => {
+        setMenuVisible(!menuVisible);
+    };
+    return (
+        <>
+            <nav>
+                <div>
+                    <img src="/logoNav.svg" />
+                </div>
+                <div className="menu">
+                    <button className="burger" onClick={toggleMenu}>
+                        <span class="material-symbols-outlined">menu</span>
+                    </button>
+                    <div className={`links ${menuVisible ? "active" : ""}`}>
+                        <Link href="#home">Home</Link>
+                        <Link href="#service">Service</Link>
+                        <Link href="#portofolio">Portofolio</Link>
+                        <Link href="#about">About</Link>
+                    </div>
+                </div>
+            </nav>
+
+
+
+        </>
+    )
+}
